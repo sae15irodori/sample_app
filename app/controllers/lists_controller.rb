bfef5c8e60b84a13a331260d98c77a3aff1b61dc
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-10 00:52:56 +0000

    Finish 第２章投稿機能を作る(投稿ボタンクリック後エラー画面出てくる)

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,21 +1,22 @@
 class ListsController < ApplicationController
   def new
-    #インスタンス変数これをビューで指定すれば入力フォーム生成される
+    #インスタンス変数.これをビューで指定すれば入力フォーム生成される
     @list = List.new
   end
-  
-  def create #createアクション追加（データ保存するためのアクション）
-    #手順１＆２（フォームに入力したデータを受け取るインスタンス）
-    
+
+   #createアクション追加（データ保存するためのアクション）
+  def create
+    #手順１＆２（フォームに入力したデータを受け取るインスタンスを生成）
+    #ビューファイルへの受け渡し不要だからローカル変数使ってる
+    #変数istにList.newを格納。list_paramsはストロングパラメータのメソッド名
     list = List.new(list_params)
-    
+
     #手順３dbに保存 (listインスタンスをdbに保存)
     list.save
-    
+
   　#手順４トップ画面へリダイレクト
-  　reairect_to '\top'
-  　
-  end
+  　redirect_to '/top'
+  end　#createアクションの定義終了
 
   def index
   end
@@ -25,4 +26,17 @@ class ListsController < ApplicationController
 
   def edit
   end
+
+ private
+  #ストロングパラメータ
+  #paramsはformから送られてくるデータ
+  #requireは送られてきたデータの中からモデル名を指定してデータ絞り込む
+  #permitはrequireで絞り込んだデータから、保存許可するカラムを指定する
+  def list_params
+    params.require(:list).permit(:title, :body)
+    #formからのデータの内、保存するモデルは(listモデル).
+    #保存するカラムは(titleとbody)
+    #リストモデルの、タイトルと本文をｄｂに保存する
+  end
 end
+end #これないとエラーになるなんで？
